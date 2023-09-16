877368762e8498ccde2f59b67637bb3dddf9a947
lemnyxx <115400742+lemnyxx@users.noreply.github.com>
2023-09-16 10:27:34 +0800

    Initial commit

---
+++ aaaa/aaaa/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            int tries = 0;
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly && tries < 5)
+            {
+                Console.Write($"Enter a number (Tries left: {5 - tries}): ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    if (number == randomNumber)
+                    {
+                        Console.WriteLine("You are correct!");
+                        guessedCorrectly = true;
+                    }
+                    else if (number < randomNumber)
+                    {
+                        Console.WriteLine("Try a higher number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try a lower number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+
+                tries++;
+            }
+
+            if (!guessedCorrectly)
+            {
+                Console.WriteLine($"You've reached the maximum number of tries. The correct number was {randomNumber}.");
+            }
+        }
+    }
+}
---
+++ functionsmethods/functionsmethods/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionsmethods
+{
+    internal class Program
+    {
+        static double EvaluateExpression(string expression)
+        {
+            string[] tokens = expression.Split(' ');
+
+            Stack<double> values = new Stack<double>();
+            Stack<char> operators = new Stack<char>();
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    values.Push(number);
+                }
+                else if (IsOperator(token))
+                {
+                    while (operators.Count > 0 && Precedence(operators.Peek()) >= Precedence(token[0]))
+                    {
+                        double b = values.Pop();
+                        double a = values.Pop();
+                        char op = operators.Pop();
+                        double result = ApplyOperation(a, b, op);
+                        values.Push(result);
+                    }
+                    operators.Push(token[0]);
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                double b = values.Pop();
+                double a = values.Pop();
+                char op = operators.Pop();
+                double result = ApplyOperation(a, b, op);
+                values.Push(result);
+            }
+
+            return values.Peek();
+        }
+
+        static bool IsOperator(string token)
+        {
+            return token == "+" || token == "-" || token == "*" || token == "/";
+        }
+
+        static int Precedence(char op)
+        {
+            if (op == '+' || op == '-')
+                return 1;
+            if (op == '*' || op == '/')
+                return 2;
+            return 0;
+        }
+
+        static double ApplyOperation(double a, double b, char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b != 0)
+                    {
+                        return a / b;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero!");
+                        return double.NaN;
+                    }
+                default:
+                    throw new InvalidOperationException("Invalid operator: " + op);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Simple Calculator");
+                Console.WriteLine("Enter an expression (e.g., '5 * 7 + 3') or 'exit' to quit: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "exit")
+                {
+                    Console.WriteLine("Exiting the calculator. Goodbye!");
+                    break;
+                }
+
+                try
+                {
+                    double result = EvaluateExpression(input);
+                    Console.WriteLine("Result: " + result);
+                    Console.WriteLine("Press Enter to continue...");
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.WriteLine("Press Enter to continue...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
